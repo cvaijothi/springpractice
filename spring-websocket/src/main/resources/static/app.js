1d1aab673bb531621bf7fa0ee180e1768a983566
cvaijothi <cvaijothi@gmail.com>
2017-07-11 18:18:52 +0530

    Spring web socket
    Test code for spring websocket which can be deployed in wildfly

---
+++ spring-websocket/src/main/resources/static/app.js
@@ -0,0 +1,53 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/nms8770-websocket/users-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/userbroker/notif', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).userDn);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+	var data = JSON.stringify({'userDn': $("#name").val(), "operationType":"CREATE"})
+
+    stompClient.send("/users/notification", {}, data);
+}
+
+function showGreeting(message) {
+	    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
